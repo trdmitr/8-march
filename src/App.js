50a7c949d49cef4277022c6579fb79afb1b900e2
trdmitr <trdmitr@gmail.com>
2023-03-05 09:58:51 +0400

    add base temlate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Routes, Route, HashRouter } from 'react-router-dom';
+import Homepage from './Components/Homepage/HomePage';
+import { Fragment, useEffect, useState } from 'react';
+import Caverpage from './Components/Caverpage/Caverpage';
+import SinglPage from './Components/SinglPage/SinglPage';
+import Papa from "papaparse";
+import { Context } from './Components/context'
+function NotFound() {
+  return <h2>Ресурс не найден</h2>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState({});
+  const [songError, setSongError] = useState('');
+  const urlParse =  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSOZR0ywpoy8jQkBV1lPCrJPlbCRMgLDQD24d1xg5fM9NYSVUksjxV8I1ub2qzDhXTGn2utDVfD6hGd/pub?output=csv";
+  useEffect (() => {
+    Papa.parse(urlParse,
+       {
+        download: true,
+        header: true,
+        complete: (results) => {
+          setData(results.data)
+        },
+        skipEmptyLines: true,
+        error: (error) => {
+          console.error(error);
+          setSongError(error)
+      }
+      })
+      }, [])
+     
+      const tributes = Array.from(data);
+      const value = tributes;
+      return (
+        <Fragment>
+          <Context.Provider value={value}>
+            <HashRouter>
+              <Routes>
+                <Route exact path="/" element={<Homepage />} />
+                <Route path="/cavers" element={<Caverpage songError={songError} />} />
+                {/* <Route path="/cavers" element={<Cavers isSongsLoading = {isSongsLoading} songs = {songs}/>} /> */}
+                <Route path='/cavers/:id' element={<SinglPage />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </HashRouter>
+          </Context.Provider>
+        </Fragment>
+ );
 }
 
 export default App;
